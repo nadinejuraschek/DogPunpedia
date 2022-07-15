f0f92b760c8521b98a452237f9383dddd58b1fd7
nadinejuraschek <nadinejwebdev@gmail.com>
2022-07-15 13:22:02 +0200

    Update: react and react-dom to v18

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,9 @@
-// REACT
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import App from './components/App';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(<App />);
